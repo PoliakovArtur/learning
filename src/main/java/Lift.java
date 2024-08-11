7a1cd0090366fa1c892dd9fa17222df03f3131b0
Pticeyabl <german.polyakov.2000@mail.ru>
2024-08-11 09:40:28 +0300

    Solved

--- src/main/java/Lift.java
+++ src/main/java/Lift.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lift {
     //
 
@@ -7,6 +9,23 @@ public class Lift {
 
      */
     public static void main(String[] args) {
-
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите начальный этаж");
+            int startFloor = scan.nextInt();
+            System.out.println("Введите конечный этаж");
+            int desiredFloor = scan.nextInt();
+            String Ending = "Приехали!";
+            if (startFloor < desiredFloor) {
+                while (startFloor != (desiredFloor + 1)) {
+                    System.out.println(startFloor++);
+                }
+            } else {
+                while (startFloor != (desiredFloor - 1)) {
+                    System.out.println(startFloor--);
+                }
+            }
+            System.out.println(Ending);
+        }
     }
 }
--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Palindrome {
     /*
         Вводится число, вывести на экран, является ли оно палиндромом.
         131 - палиндром
         132 - не палиндром
      */
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Введите число");
+            Scanner scan = new Scanner(System.in);
+            int number = scan.nextInt();
+            int mod, div, invNumber = 0;
+            div = number;
+            while (div != 0) {
+                mod = div % 10;
+                div = div / 10;
+                invNumber = invNumber * 10 + mod;
+            }
+            if (number == invNumber) {
+                System.out.println("Число является палиндромом");
+            } else {
+                System.out.println("Число не является палиндромом");
+            }
+        }
+    }
 }
